dc85fe36ff4329c3189c213a78203aaa5ba2875a
Kevin Ho <ho.kevins@gmail.com>
2023-01-18 12:52:35 -0500

    fix: implement naive fix for Refresh button

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,11 @@ import styles from './footer.module.css';
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <Link className={styles.refresh} href="/">Refresh</Link>
+      {/* Used when doing page-level data fetching */}
+      {/*<Link className={styles.refresh} href="/">Refresh</Link>*/}
 
       {/* Used when doing component-level data fetching */}
-      {/*<a className={styles.refresh} onClick={() => window.location.reload()} href="/">Refresh 2</a>*/}
+      <a className={styles.refresh} onClick={() => window.location.reload()} href="/">Refresh</a>
 
     </div>
   );
