cd081cf4e5f85a1447c95d1bff7e0729e3827aca
Kevin Ho <ho.kevins@gmail.com>
2023-01-20 16:37:16 -0500

    chore: move SyncLoader to Customer component

--- components/customers.js
+++ components/customers.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from './customers.module.css';
 import { useState, useEffect } from 'react';
 import { getCustomersData } from '../lib/customers'
+import { SyncLoader } from 'react-spinners';
 
 export default function Customers({ allCustomersData }) {
 
@@ -21,37 +22,44 @@ export default function Customers({ allCustomersData }) {
   */
 
   return (
-    <ul className={styles.list}>
-      {
-        allCustomersData && allCustomersData.map(customer => {
-          return (
-            <li className={styles.card} key={customer.login.uuid}>
-              <Image
-                priority
-                className={styles.picture}
-                src={customer.picture.medium}
-                height={72}
-                width={72}
-                alt=""
-              />
-              <div className={styles.data}>
-                <div className={`${styles.name} ${styles.capitalize}`}>
-                  {customer.name.first} {customer.name.last}
+    allCustomersData.length ?
+      <ul className={styles.list}>
+        {
+          allCustomersData.map(customer => {
+            return (
+              <li className={styles.card} key={customer.login.uuid}>
+                <Image
+                  priority
+                  className={styles.picture}
+                  src={customer.picture.medium}
+                  height={72}
+                  width={72}
+                  alt=""
+                />
+                <div className={styles.data}>
+                  <div className={`${styles.name} ${styles.capitalize}`}>
+                    {customer.name.first} {customer.name.last}
+                  </div>
+                  <div className={styles.capitalize}>
+                    {customer.location.street.number} {customer.location.street.name}
+                  </div>
+                  <div className={styles.capitalize}>
+                    {customer.location.city}, {customer.location.state}
+                  </div>
+                  <a className={styles.email} href={`mailto:${customer.email}`}>
+                    {customer.email}
+                  </a>
                 </div>
-                <div className={styles.capitalize}>
-                  {customer.location.street.number} {customer.location.street.name}
-                </div>
-                <div className={styles.capitalize}>
-                  {customer.location.city}, {customer.location.state}
-                </div>
-                <a className={styles.email} href={`mailto:${customer.email}`}>
-                  {customer.email}
-                </a>
-              </div>
-            </li>
-          );
-        })
-      }
-    </ul>
+              </li>
+            );
+          })
+        }
+      </ul>
+    :
+      <SyncLoader
+        color="#1bb1dc"
+        loading={true}
+        style={{ margin: 20 }}
+      />
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 import Footer from '../../components/footer'
 import Customers from '../../components/customers'
-import { SyncLoader } from 'react-spinners';
 
 // This will ensure data is passed as props from Home -> Customers
 // Alternatively, see components/customers.js to see component-level data fetching
@@ -21,32 +20,20 @@ export async function getStaticProps() {
 
 export default function Home({ allCustomersData }) {
   return (
-    allCustomersData.length === 4 ?
-      <>
-        <Head>
-          <title>Welcome to Haven Life!</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <main>
-          <div className="root-container">
-            <div className={styles.container}>
-              <div className={styles.title}>Our Newest Customers</div>
-              <Customers allCustomersData={allCustomersData} />
-              <Footer />
-            </div>
+    <>
+      <Head>
+        <title>Welcome to Haven Life!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div className="root-container">
+          <div className={styles.container}>
+            <div className={styles.title}>Our Newest Customers</div>
+            <Customers allCustomersData={allCustomersData} />
+            <Footer />
           </div>
-        </main>
-      </>
-    :
-      <div className="loading-state">
-        <SyncLoader
-          color="#1bb1dc"
-          loading={true}
-        />
-        Loading...
-        <div style={{ color: '#1bb1dc', fontSize : 22 }}>
-          Error Loading Data
         </div>
-      </div>
+      </main>
+    </>
   );
 }
