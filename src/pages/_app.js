60af7e6b195855fa474ecd20fee894fc0cbbf137
Kevin Ho <ho.kevins@gmail.com>
2023-06-26 10:23:52 -0400

    chore(GitHub): remove a test comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import ErrorBoundary from '../../components/ErrorBoundary'
 import '@/styles/globals.css'
 
-// A test comment for GitHub.
-
 export default function App({ Component, pageProps }) {
   return (
     <ErrorBoundary>
